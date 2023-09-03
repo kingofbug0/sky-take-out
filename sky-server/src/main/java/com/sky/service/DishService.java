3a70df70a3a062b9ada09007de9af5134dbd6da2
messi <barca10messi@163.com>
2023-09-04 03:21:55 +0800

    第十三次代码-商品代码浏览业务(以及微信小程序的使用)

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -54,4 +54,10 @@ public interface DishService
      * @param id
      */
     void StopOrStart(Integer status, Long id);
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
